2e7504a25ee4add796134339dfa6647f8af1bab8
oaxhack <95191846+oaxhack@users.noreply.github.com>
2024-02-27 06:16:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -148,3 +148,10 @@ $(function() {
 
 	init();
 });
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
